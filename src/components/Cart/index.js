01437a206b8333bcb624d1ac7cf0f7ac75151dcd
MohanRanga815 <mohanranga320@gmail.com>
2023-06-20 07:13:13 +0000

    shopApp

---
+++ src/components/Cart/index.js
@@ -0,0 +1,17 @@
+// Write your JS code here
+import Header from '../Header'
+import './index.css'
+
+const Cart = () => (
+  <>
+    <Header />
+    <div className="cart-con">
+      <img
+        src="https://assets.ccbp.in/frontend/react-js/nxt-trendz-cart-img.png"
+        alt="cart"
+        className="image"
+      />
+    </div>
+  </>
+)
+export default Cart
